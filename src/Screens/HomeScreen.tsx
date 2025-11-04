d8939df5fcd452e992df0253d5ba5dab9aead176
pro1001000r <pro1001000r@ya.ru>
2025-11-04 12:08:40 +0300

    123

--- src/Screens/HomeScreen.tsx
+++ src/Screens/HomeScreen.tsx
@@ -1,14 +1,17 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 
 // import CanvasVit from "../Components/CanvasVit";
 import { Card, Col, Container, Image, Row } from "react-bootstrap";
 
 import Logo from "../Template/images/LogoPikclick512.png";
-import NoImg from "../Template/images/no-image.png";
-import MyFoto from "../Template/images/Fon12.png";
-import TestVit from "../Components/TestVit";
+import ButtonVit from "../Components/ButtonVit";
+import ModalVit from "../Components/ModalVit";
+import ScanerVit from "../Components/ScanerVit";
 
 const HometScreen: FC = () => {
+  const [scan, setScan] = useState<string>("Просканируйте штрихкод...");
+  const [show, setShow] = useState(false);
+
   return (
     <>
       <Container>
@@ -21,27 +24,54 @@ const HometScreen: FC = () => {
                 alt="Логотип"
                 className="vitImageContain vShadowImg"
               />
-              <Image
-                src={MyFoto}
-                alt="Логотип"
-                className="vitImageContain vShadowImg"
-              />
 
-              <Image
-                src={NoImg}
-                alt="Логотип"
-                className="vitImageContain vShadowImg"
-              />
               <p className="vit-font-propis">
-                Инвентаризация?!
+                Скоро инвентаризация?! <br />
+                Как посчитать?!
                 <br />
                 <small> - легко и просто</small>
               </p>
             </h1>
-            <p className="text-center vit-font-verdana">
-              Сайт предназначен для руководителей, бизнесменов и всех, кому
-              нужно точное представление о состоянии товаров на складах
-            </p>
+
+            <Row className="text-center">
+              <Col>
+                <p>{scan}</p>
+                <ButtonVit
+                  className=" btn-primary"
+                  icon="UpcScan"
+                  name="Сканер-то в руках держите уже"
+                  onClick={() => setShow(true)}
+                />
+
+                <ModalVit show={show} setShow={setShow}>
+                  <ScanerVit setScan={setScan} />
+                </ModalVit>
+              </Col>
+            </Row>
+            <Row>
+              <h4>Для кого этот сервис:</h4>
+              <p className="text-center vit-font-verdana">
+                для кладовщиков, маркетологов и всем, кому нужно точное
+                представление о состоянии товаров на складах
+              </p>
+            </Row>
+            <Row>
+              <h4>Для чего этот сервис:</h4>
+              <p className="text-center vit-font-verdana">
+                <b>Подсчёт товаров с мобильного</b>
+                <br />
+                (инвентаризация)
+                <br />
+                <b>Приведение в порядок карточек товаров</b>
+                <br />
+                (фото, описание, штрихкодирование, печать этикеток, ценовая
+                политика)
+                <br />
+                Создание карточки товара при Внедрение новой какой-либо учетной
+                системы (1С, Saby, Эватор) и первоначальных остатков
+              </p>
+            </Row>
+
             <br />
           </Col>
         </Row>
